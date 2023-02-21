2fe16feb7930748d44a1e1e33d53932d7310f059
Cyubahiroprince <cyubahiroprince2222@gmail.com>
2023-02-20 16:12:10 -0800

    C program print all single number of base 10 from used char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Desciprtion: all single Digit of base 10 starting from 0 used char
+ * Return: always 0 (success)
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n % 10) + '0');
+	putchar("\n");
+	return (0);
+}
