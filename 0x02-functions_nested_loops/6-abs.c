a3fc50322bed55501b85046d3da7f87a8c9365d4
Cyubahiroprince <cyubahiroprince2222@gmail.com>
2023-02-22 14:09:47 -0800

    function that computes the absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* _abc - computes the absolute value of an interger.
+* _abs - computes the absolute value of an interger.
 *
-* @c: the number to be computed 
+* @c: the number to be computed
 * Return: Absolute value of number or zero
 */
-int _abc(int c)
-{
-	if (c < 0)
-	{
-		int abc_val;
 
-		abs_val = c * -1;
-		return (abs_val);
+int _abs(int c)
+
+{
+	if (c > 0)
+	{
+		_putchar(c);
+		return (c);
 	}
-	return (c);
+
+	else
+	{
+		_putchar(c);
+		return (-c);
+	}
 }
+
+
