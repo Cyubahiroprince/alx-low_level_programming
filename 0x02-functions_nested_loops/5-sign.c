013e09509870a42339575a34f8a3fa4f23f2a795
Cyubahiroprince <cyubahiroprince2222@gmail.com>
2023-02-22 09:27:49 -0800

    function that checks for alphabetic character

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,26 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * print_sign - print the  sign of number
  *@n: the number to be checked
  *Return: 1 for possitive num, -1 for negative num or 0 for anything else
  */
+
 int print_sign(int n)
+
 {
 	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
 	}
 	else if (n < 0)
 	{
-		_putchar(45);
+		_putchar('-');
 		return (-1);
-	}
-	else
-	{
-		_putchar(48);
-		return (0);
 	}
 }
