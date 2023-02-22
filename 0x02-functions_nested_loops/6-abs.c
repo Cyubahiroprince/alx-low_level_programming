2b988ebcad8192804cee966420a771465c193d9d
Cyubahiroprince <cyubahiroprince2222@gmail.com>
2023-02-22 07:12:27 -0800

    function that checks for alphabetic character

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _abc - computes the absolute value of an interger.
+*
+* @c: the number to be computed 
+* Return: Absolute value of number or zero
+*/
+int _abc(int c)
+{
+	if (c < 0)
+	{
+		int abc_val;
+
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+}
