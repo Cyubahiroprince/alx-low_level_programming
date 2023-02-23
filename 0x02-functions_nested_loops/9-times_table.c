25f3d35e256c93afebda9aa820e2e88d80073279
Cyubahiroprince <cyubahiroprince2222@gmail.com>
2023-02-22 16:13:15 -0800

    a function that prints every minute of the day of Jack Bauer

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * times_table - print the 9 times table, starting with 0
+ */
+
+void times_table(void)
+{
+	int x, y, z, a, b;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			if (z > 9)
+
+			{
+				a = z % 10;
+				b = (z - a) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(b + '0');
+				_puchar(a + '0');
+			}
+
+			_putchar(z + '0');
+		}
+	}
+	_putchar('\n');
+}
