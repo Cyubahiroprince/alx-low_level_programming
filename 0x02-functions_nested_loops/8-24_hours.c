5f680b992a8ba635c936c3cb86379475fea1cc12
Cyubahiroprince <cyubahiroprince2222@gmail.com>
2023-02-22 18:00:26 -0800

    function that prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,11 +12,11 @@ void jack_bauer(void)
 	{
 		for (min = 0; min < 60; min++)
 		{
-			_putchar((hours / 10) + 0);
-			_putchar((hours % 10) + 0);
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
 			_putchar(':');
-			_putchar((min / 10) + 0);
-			_putchar((min % 10) + 0);
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
 				_putchar ('\n');
 		}
 	}
