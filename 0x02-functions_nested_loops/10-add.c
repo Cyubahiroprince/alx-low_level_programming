6431fa4d6ff6308320ddb2db87d6ee7ecdd79d98
Cyubahiroprince <cyubahiroprince2222@gmail.com>
2023-02-22 17:26:40 -0800

    function that adds two integers and returns the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,7 +3,7 @@
  * add - function that adds two integer and returns the result
  * @num1:number to be add
  * @num2 :number to be add
- * @sum : print output
+ * Return: sum
  */
 
 int add(int num1, int num2)
