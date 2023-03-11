27f9e1bad72313781f0d4802a8dc8466ebd7a532
Cyubahiroprince <cyubahiroprince2222@gmail.com>
2023-03-11 14:10:12 -0800

    Tasks 0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/6-abc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _abs - computes the absolute value of an interger.
+*
+* @c: the number to be computed
+* Return: Absolute value of number or zero
+*/
+
+int _abs(int c)
+
+{
+	if (c > 0)
+	{
+		return (c);
+	}
+
+	else
+	{
+		return (-c);
+	}
+}
