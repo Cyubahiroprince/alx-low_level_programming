7da0d1e11ac3391a725bcd4be975c1a7635e5f01
Cyubahiroprince <cyubahiroprince2222@gmail.com>
2023-03-15 14:31:31 -0700

    program that multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,46 +1,34 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- * Return: the int converted from the string
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
  */
 int _atoi(char *s)
 {
-	int i, n, d, len, f, digit;
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
 
-	i = 0;
-	n = 0;
-	d = 0;
-	len = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
 	{
 	if (s[i] == '-')
-		++d;
-
+	sign *= -1;
 	if (s[i] >= '0' && s[i] <= '9')
 	{
-	digit = s[i] - '0';
-	if (d % 2)
-	digit = -digit;
-	n = n * 10 + digit;
-	f = 1;
-	if (s[i + 1] < '0' || s[i + 1] > '9')
-		break;
-	f = 0;
+	flag = 1;
+	result *= 10;
+	result += (s[i] - '0');
 	}
-	i++;
+	else if (flag == 1)
+	flag = 2;
 	}
-	if (f == 0)
-	return (0);
-	return (n);
+	if (sign == -1)
+	output = -result;
+	else
+	output = result;
+	return (output);
 }
-
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
