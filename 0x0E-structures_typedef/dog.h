1b71038ace85f129cb3c800a6ec39e8fefc65fab
Cyubahiroprince <cyubahiroprince2222@gmail.com>
2023-03-21 17:32:52 -0700

    a struct represents a dog and contains information

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - represents a dog
+ * @name: pointer to name of the dog
+ * @age: age of the dog
+ * @owner: pointer to name of the dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
