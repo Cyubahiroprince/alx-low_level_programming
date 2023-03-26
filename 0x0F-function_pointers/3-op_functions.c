57a4fa99734b8ae0ce0c13a44b36064795544ba6
Cyubahiroprince <cyubahiroprince2222@gmail.com>
2023-03-26 09:03:19 -0700

    a program that performs simple of all operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,15 @@
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The product of a and b.
+ */
+
+int op_add(int a, int b){return (a+b);}
+int op_sub(int a, int b){return (a - b);}
+int op_mul(int a, int b){return (a * b);}
+int op_div(int a, int b){return (a / b);}
+int op_mod(int a, int b){return (a % b);}
